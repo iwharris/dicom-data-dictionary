f12fea1ae87d9c18a279d5f2809205cf89fb1109
Ian Harris <ianwebsterharris@gmail.com>
2019-12-28 15:47:33 -0700

    Add a full-build script that fetches and builds a specific revision of the DICOM data dictionary

---
+++ scripts/full-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Terminate on any error
+set -e
+
+# DICOM revision; eg. "2014a" (default: "current")
+REVISION=${1:-current}
+BASE_DIR="./downloaded"
+OUT_DIR="${BASE_DIR}/${REVISION}"
+XML_NAME="part06.xml"
+
+# Download file
+scripts/fetch.sh "${REVISION}" "${BASE_DIR}"
+
+# Parse file and generate ts module
+npm run -s build ${OUT_DIR}/${XML_NAME}
+
+# Run tests and lint
+npm run -s prettier
+npm test
