0e6294383f2cacae5c66864eae10b8eb259d9912
Ian Harris <ianwebsterharris@gmail.com>
2019-12-28 15:47:09 -0700

    Parameterize fetch script with an output directory; use original XML filename to take advantage of wget's -N option

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -2,9 +2,10 @@
 
 # DICOM revision; eg. "2014a" (default: "current")
 REVISION=${1:-current}
-TEMP_DIR=./downloaded
+BASE_DIR=${2:-"./downloaded"}
+OUT_DIR="${BASE_DIR}/${REVISION}"
 
-mkdir -p "${TEMP_DIR}"
+mkdir -p ${BASE_DIR}
 
 # Download the file to the temp directory
-wget -N -O "${TEMP_DIR}/${REVISION}.xml" "http://dicom.nema.org/medical/dicom/${REVISION}/source/docbook/part06/part06.xml"
+wget -N -P ${OUT_DIR} "http://dicom.nema.org/medical/dicom/${REVISION}/source/docbook/part06/part06.xml"
